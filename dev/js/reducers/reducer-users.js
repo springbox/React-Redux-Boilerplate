b45115f392196fdff576e41f30dac756bded4ce1
ben.balentine <myrcutio@gmail.com>
2017-09-01 23:59:13 -0500

    Update reducer-users.js

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -7,27 +7,11 @@ export default function () {
     return [
         {
             id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
+            first: "Test",
+            last: "User",
+            age: 42,
+            description: "Testing autoscaling deploy of a react app",
+            thumbnail: "https://i.pinimg.com/originals/a3/5e/8e/a35e8e7a3bb143868544c3a348aa94a0.jpg"
         }
     ]
 }
